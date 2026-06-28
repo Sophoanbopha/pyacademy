bfea5f8c2268923f04b2fe97676ec853881401e7
Sophoan Bopha SON <sophoanbopha2004@gmail.com>
2026-06-28 23:44:47 +0700

    Fix GitHub Pages deployment

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "053ec6c6f21f8f60cb9f3ccbd82371e7
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "3dc6962de4ab24f8e4f226935da1979f",
+"flutter_bootstrap.js": "17ad5263282eeb11fdcebad51d3af789",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
